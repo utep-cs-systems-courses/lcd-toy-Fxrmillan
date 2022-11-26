47be54a819fefb8ed539835adbf03d5eba12181d
Fxrmillan <fxrmillan13@outlook.com>
2022-11-26 15:40:54 -0700

    commiting for tests

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -16,8 +16,21 @@ main()
 
   clearScreen(COLOR_BLUE);
 
-  drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+  //  drawString5x7(20,20, "Ape", COLOR_GREEN, COLOR_RED);
 
-  fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  // fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+
+  int col =40, row =40;
+  for(int i =0;i<20;i++)
+    for(int j=0;j<i;j++){
+      drawPixel(col+i ,row+j , COLOR_BLACK);
+      drawPixel(col+i,row-j,COLOR_RED);
+    }
+  col+=40;  
+  for(int i=20;i>0;i--)
+    for(int j=0;j<i;j++){
+      drawPixel(col-i,row-j,COLOR_GREEN);
+      drawPixel(col-i,row+j,COLOR_GREEN);
+    }
   
 }
